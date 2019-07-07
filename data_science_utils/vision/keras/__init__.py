bfbed7393db7b9e12ad9fc36099481bd94ef2481
amitkayal <amitkayal@outlook.com>
2019-07-07 11:37:01 +0530

    Update __init__.py

--- data_science_utils/vision/keras/__init__.py
+++ data_science_utils/vision/keras/__init__.py
@@ -322,14 +322,14 @@ def show_misclassified(X, Y_ohe, Y_pred, classes,
 
 
     def plot_figures(figures, nrows = 1, ncols=1):
-    """Plot a dictionary of figures.
-
-    Parameters
-    ----------
-    figures : <title, figure> dictionary
-    ncols : number of columns of subplots wanted in the display
-    nrows : number of rows of subplots wanted in the figure
-    """
+ #   """Plot a dictionary of figures.
+#
+ #   Parameters
+  #  ----------
+   # figures : <title, figure> dictionary
+    #ncols : number of columns of subplots wanted in the display
+    #nrows : number of rows of subplots wanted in the figure
+    #"""
 
     fig, axeslist = plt.subplots(ncols=ncols, nrows=nrows,squeeze =False,figsize=(15, 15))
     for ind,title in enumerate(figures):
